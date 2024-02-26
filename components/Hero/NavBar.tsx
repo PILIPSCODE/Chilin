bf8abd67e0a13680b1d30f18f6d3c62e26e30bb0
Pilipus <Pilipus@gmail.com>
2024-02-26 20:14:41 +0700

    del: Drag and drop navigation.

--- components/Hero/NavBar.tsx
+++ components/Hero/NavBar.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import React, { useState } from 'react'
-import { FaCartShopping, FaPhone, FaX } from "react-icons/fa6"
-import { MdDashboard } from "react-icons/md"
+import { FaBars, FaCartShopping, FaPhone, FaX } from "react-icons/fa6"
+import { CgYinyang } from "react-icons/cg";
 import { useEffect } from 'react'
 import { useAppSelector } from '@/redux/store'
 import Image from 'next/image'
@@ -16,8 +16,6 @@ const NavBar = () => {
     const [onscroll, setonScrool] = useState(false)
     const jmlhCart = useAppSelector((state) => state.CartReducer.jmlh);
     const [Qty, Setjmlhcart] = useState(0)
-    const [position, SetPosition] = useState({x:48, y:192})
-    console.log(position)
 
     useEffect(() => {
         Setjmlhcart(Number(jmlhCart))
@@ -36,43 +34,42 @@ const NavBar = () => {
     }, [])
 
 
-    
+
     return (
         <div className={`flex justify-center  ${onscroll ? "backdrop-blur-md bg-black/45" : "bg-transparent"}  font-Poppins navb  fixed w-full h-20 items-center`}>
             <div className='w-9/12 text-white flex justify-between  '>
-                <Link href={"/"} className='flex gap-2 items-center text-2xl font-Abril'>
+                <Link href={"/"} className='flex gap-2 items-center relative z-20 max-sm:text-base text-2xl font-Abril'>
                     <Image className='rounded-full' src="/logo.png" width={50} height={50} alt='logo' />
-                    <h1>Chillin</h1>
+                    <h1 className='typo'>Chillin</h1>
                 </Link>
-                <div style={{left:position.x -50 ,top:position.y -44}} className={`${showNav ? "max-md:absolute " : "max-md:hidden"} right-5 flex-col flex  justify-center items-center  top-36 max-md:bg-white max-md:h-36 max-md:w-36 rounded-full  text-white`}>
 
-                    <div className='flex gap-7 justify-center items-center w-full h-1/2 max-md:border-b-2 border-black'>
-                        <Link href={'/#about'} className='my-auto pt-3'>
-                            <Image className='bg-white md:hidden rounded-full' src="/logo.png" width={40} height={40} alt='logo' />
-                            <h1 className='max-md:hidden'>About Us</h1>
-                        </Link>
-                        <div className='my-auto pt-3'>
-                            <div className='text-4xl text-black md:hidden'><FaPhone /></div>
-                            <h1 className='max-md:hidden'>Contact Us</h1>
-                        </div>
-                    </div>
-                    <div className='flex gap-7 justify-center items-center w-full  h-1/2 max-md:border-t-2 border-black'>
 
-                    </div>
-
-                </div>
-                <div draggable onDragEnd={(e) => {e.preventDefault(),SetPosition({x:e.clientX,y:e.clientY})}}    style={{left:position.x,top:position.y}} className={`absolute cursor-move    justify-center shadow-3xl  flex items-center text-white bg-black border w-12 h-12 md:hidden rounded-full`}>
-                    <div className={`h-full w-full ${!showNav ? "hidden" : "flex justify-center items-center"}`} onClick={() => setShowNav(false)}>
-                        <FaX />
-                    </div>
-                    <div  onClick={() => setShowNav(true)} className={`h-full w-full ${!showNav ? "flex justify-center items-center" : "hidden"}`}>
-                        <MdDashboard />
+                <div className={`${showNav? "opacity-100 z-10 top-20 ":"opacity-0 z-0 -top-20"} duration-700 flex gap-7 max-md:absolute justify-center md:items-center rounded-lg border   w-9/12 max-md:bg-black py-1 md:h-20`}>
+                    <Link href={'/#about'} className=' flex gap-2 justify-center items-center'>
+                        <CgYinyang />
+                        <h1 >About Us</h1>
+                    </Link>
+                    <div className=' flex gap-2 justify-center items-center'>
+                        <FaPhone />
+                        <h1>Contact Us</h1>
                     </div>
                 </div>
-                <div className='flex gap-9 items-center'>
+
+
+
+
+                <div className='flex gap-9 items-center relative'>
                     <Link className='flex items-center gap-1' href={'/Cart'}><FaCartShopping /><span className='text-white absolute translate-x-6'>{String(Qty)}</span></Link>
 
                     <h1 className='bg-white text-black p-2 rounded-lg hover:bg-black hover:text-white hover:border-2 border-2'>Variant</h1>
+                <div onClick={() => setShowNav(!showNav)} className='md:hidden absolute -right-7'>
+                    {showNav?
+                    <FaX/>
+                    :
+                    <FaBars />
+
+                }
+                </div>
                 </div>
             </div>
         </div>
